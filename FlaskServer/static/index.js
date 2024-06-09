fc2332eccee323c99e9327e445d0a5dfd0d1d8b5
Teresa Yang <yangte@sheridancollege.ca>
2024-06-09 15:47:07 -0400

    Changed audio type received from "get_random_audio" function.

--- FlaskServer/static/index.js
+++ FlaskServer/static/index.js
@@ -10,6 +10,74 @@ random_button_element.onclick = handleRandomButtonClick;
 let prompted_audio_element = document.getElementById("prompted_audio");
 let recorded_audio_element = document.getElementById("recorded_audio");
 
+
+var accuracyscore = document.getElementById('accuracyscore');
+var fluencyscore = document.getElementById('fluencyscore');
+var completenessscore = document.getElementById('completenessscore');
+var pronscore = document.getElementById('pronscore');
+var wordsomitted = document.getElementById('wordsomitted');
+var wordsinserted = document.getElementById('wordsinserted');
+var omittedwords = "";
+var insertedwords = "";
+wordsinserted.style.display = "none";
+document.getElementById("wih").style.display = "none";
+
+var wordrow = document.getElementById('wordrow');
+var phonemerow = document.getElementById('phonemerow');
+var scorerow = document.getElementById('scorerow');
+var lastgettstext;
+var objectUrlMain;
+var wordaudiourls = new Array;
+
+var phthreshold1 = 80;
+var phthreshold2 = 60;
+var phthreshold3 = 40;
+var phthreshold4 = 20;
+
+var AudioContext = window.AudioContext || window.webkitAudioContext;;
+var audioContent;
+var start = false;
+var stop = false;
+var permission = false;
+var reftextval;
+var gumStream; 						//stream from getUserMedia()
+var rec; 							//Recorder.js object
+var audioStream ; 					//MediaStreamAudioSourceNode we'll be recording
+var blobpronun;
+var offsetsarr;
+var tflag = true;
+var wordlist;
+
+var t0 = 0;
+var t1;
+var at;
+
+window.onload = () => {
+    if(tflag){
+        tflag = gettoken();
+        tflag = false;
+    }
+    
+};
+
+function gettoken() {
+    var request = new XMLHttpRequest();
+    request.open('POST', '/gettoken', true);
+
+    // Callback function for when request completes
+    request.onload = () => {
+        // Extract JSON data from request
+        const data = JSON.parse(request.responseText);
+        at = data.at;
+    }
+
+    //send request
+    request.send();
+    return false;
+}
+
+
+
 let audio_name = '';
 let record = null;
 let audioChunks = [];
@@ -33,7 +101,6 @@ function handleRandomButtonClick() {
 
   start_button_element.disabled = false;
 
-  // Ajax call to /get_random_line endpoint
   $.ajax({
     type: 'GET',
     url: '/get_random_line',
@@ -48,7 +115,8 @@ function handleRandomButtonClick() {
         .then(response => response.arrayBuffer())
         .then(buffer => {
           // Create Blob object from array buffer
-          const blob = new Blob([buffer], { type: "audio/mpeg-3" });
+          const blob = new Blob([buffer], { type: "audio/wav" });
+        //   const blob = new Blob([buffer], { type: "audio/mpeg-3" });
 
           // Set source of audio element to Blob URL
           prompted_audio_element.src = URL.createObjectURL(blob);
@@ -85,8 +153,8 @@ function handleStartButtonClick() {
 }
 
 navigator.mediaDevices
-    .getUserMedia({ audio: true })
-    .then(stream => {
+.getUserMedia({ audio: true })
+.then(stream => {
         //create new recorder object
         record = new MediaRecorder(stream);
 
@@ -95,7 +163,7 @@ navigator.mediaDevices
             audioChunks.push(e.data);
             //once recorder is done send data to server
             if (record.state === "inactive") {
-                let blob = new Blob(audioChunks, { type: 'audio/mpeg-3' });
+                let blob = new Blob(audioChunks, { type: 'audio/wave' });
 
                 //enabled audio playback
                 recorded_audio_element.src = URL.createObjectURL(blob);
@@ -104,7 +172,7 @@ navigator.mediaDevices
 
                 //create file to send to server
                 const form = new FormData();
-                form.append('file', blob, audio_name + ".mp3");
+                form.append('file', blob, audio_name + ".wav");
                 $.ajax({
                     type: 'POST',
                     url: '/save-record',
@@ -119,4 +187,152 @@ navigator.mediaDevices
 
             }
         }
-    });
+});
+
+var soundAllowed = function (stream) {
+    permission = true;
+    audioContent = new AudioContext();
+    gumStream = stream;
+    audioStream = audioContent.createMediaStreamSource(stream);
+    rec = new Recorder(audioStream, { numChannels: 1 })
+
+    //start the recording process
+    rec.record()
+}
+    
+var soundNotAllowed = function (error) {
+    h.innerHTML = "You must allow your microphone.";
+    console.log(error);
+}
+
+//function for handling main button clicks
+document.getElementById('buttonmic').onclick = function () {
+
+    if (reftext.value.length == 0) {
+        alert("Reference Text cannot be empty!");
+    }
+    else {
+        if (stop) {
+            window.location.reload();
+        }
+        else if (start) {
+
+            start = false;
+            stop = true;
+            this.innerHTML = "<span class='fa fa-refresh'></span>Refresh";
+            this.className = "green-button";
+            rec.stop();
+
+            //stop microphone access
+            gumStream.getAudioTracks()[0].stop();
+
+            //create the wav blob and pass it on to createDownloadLink
+            rec.exportWAV(createDownloadLink);
+        }
+        else {
+            if (!permission) {
+                navigator.mediaDevices.getUserMedia({ audio: true })
+                    .then(soundAllowed)
+                    .catch(soundNotAllowed);
+            }
+
+            start = true;
+            reftext.readonly = true;
+            reftext.disabled = true;
+            ttbutton.disabled = true;
+            ttbutton.className = "btn";
+            reftextval = reftext.value;
+
+            this.innerHTML = "<span class='fa fa-stop'></span>Stop";
+            this.className = "red-button";
+        }
+    }
+};
+
+
+function fillDetails(words) {
+    for (var wi in words) {
+        var w = words[wi];
+        var countp = 0;
+
+        if (w.ErrorType == "Omission") {
+            omittedwords += w.Word;
+            omittedwords += ', ';
+
+            var tdda = document.createElement('td');
+            tdda.innerText = '-';
+            phonemerow.appendChild(tdda);
+
+            var tddb = document.createElement('td');
+            tddb.innerText = '-';
+            scorerow.appendChild(tddb);
+
+            var tdw = document.createElement('td');
+            tdw.innerText = w.Word;
+            tdw.style.backgroundColor = "orange";
+            wordrow.appendChild(tdw);
+        }
+        else if (w.ErrorType == "Insertion") {
+            insertedwords += w.Word;
+            insertedwords += ', ';
+        }
+        else if (w.ErrorType == "None" || w.ErrorType == "Mispronunciation") {
+            for (var phonei in w.Phonemes) {
+                var p = w.Phonemes[phonei]
+
+                var tdp = document.createElement('td');
+                tdp.innerText = p.Phoneme;
+                if (p.AccuracyScore >= phthreshold1) {
+                    tdp.style.backgroundColor = "green";
+                }
+                else if (p.AccuracyScore >= phthreshold2) {
+                    tdp.style.backgroundColor = "lightgreen";
+                }
+                else if (p.AccuracyScore >= phthreshold3) {
+                    tdp.style.backgroundColor = "yellow";
+                }
+                else {
+                    tdp.style.backgroundColor = "red";
+                }
+                phonemerow.appendChild(tdp);
+
+                var tds = document.createElement('td');
+                tds.innerText = p.AccuracyScore;
+                scorerow.appendChild(tds);
+                countp = Number(phonei) + 1;
+            }
+            var tdw = document.createElement('td');
+            tdw.innerText = w.Word;
+            var x = document.createElement("SUP");
+            var t = document.createTextNode(w.AccuracyScore);
+            x.appendChild(t);
+            tdw.appendChild(x);
+            tdw.colSpan = countp;
+            if (w.ErrorType == "None") {
+                tdw.style.backgroundColor = "lightgreen";
+            }
+            else {
+                tdw.style.backgroundColor = "red";
+            }
+            wordrow.appendChild(tdw);
+        }
+
+    }
+}
+
+function fillData(data) {
+
+    document.getElementById("summarytable").style.display = "flex";
+    accuracyscore.innerText = data.AccuracyScore;
+    fluencyscore.innerText = data.FluencyScore;
+    completenessscore.innerText = data.CompletenessScore;
+    pronscore.innerText = parseInt(data.PronScore, 10);
+
+    fillDetails(data.Words);
+    wordsomitted.innerText = omittedwords;
+    if (insertedwords != "") {
+        document.getElementById("wih").style.display = "block";
+        wordsinserted.style.display = "block";
+        wordsinserted.innerText = insertedwords;
+    }
+}
--- FlaskServer/web.py
+++ FlaskServer/web.py
@@ -1,11 +1,11 @@
 import os
 import random
 from pathlib import Path
+import requests
+import base64
+import azure.cognitiveservices.speech as speechsdk
 
-from flask import Flask, flash, send_from_directory
-from flask import redirect
-from flask import render_template
-from flask import request
+from flask import Flask, flash, send_from_directory, redirect, render_template, request, jsonify
 from flask_session import Session
 from librosa import load
 
@@ -25,6 +25,83 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 sess.init_app(app)
 
+subscription_key = 'b36e4615cce844da8cd0e61f650bef9e'
+region = "eastus"
+language = "en-US"
+voice = "Microsoft Server Speech Text to Speech Voice (en-US, JennyNeural)"
+
+
+# Home page, render the "index.html" template
+@app.route('/')
+def home():
+    return render_template('index.html', name=None)
+
+# Dummy response to satisfy website if it does get request to .../favicon.ico
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    return '<h1></h1>'
+
+@app.route("/gettoken", methods=["POST"])
+def gettoken():
+    fetch_token_url = 'https://%s.api.cognitive.microsoft.com/sts/v1.0/issueToken' %region
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key
+    }
+    response = requests.post(fetch_token_url, headers=headers)
+    access_token = response.text
+    return jsonify({"at":access_token})
+
+
+
+@app.route("/ackaud", methods=["POST"])
+def ackaud():
+    f = request.files['audio_data']
+    reftext = request.form.get("engish")
+    # reftext = request.form.get("reftext")
+    #    f.save(audio)
+    #print('file uploaded successfully')
+
+    # a generator which reads audio data chunk by chunk
+    # the audio_source can be any audio input stream which provides read() method, e.g. audio file, microphone, memory stream, etc.
+    def get_chunk(audio_source, chunk_size=1024):
+        while True:
+            #time.sleep(chunk_size / 32000) # to simulate human speaking rate
+            chunk = audio_source.read(chunk_size)
+            if not chunk:
+                #global uploadFinishTime
+                #uploadFinishTime = time.time()
+                break
+            yield chunk
+
+    # build pronunciation assessment parameters
+    referenceText = reftext
+    pronAssessmentParamsJson = "{\"ReferenceText\":\"%s\",\"GradingSystem\":\"HundredMark\",\"Dimension\":\"Comprehensive\",\"EnableMiscue\":\"True\"}" % referenceText
+    pronAssessmentParamsBase64 = base64.b64encode(bytes(pronAssessmentParamsJson, 'utf-8'))
+    pronAssessmentParams = str(pronAssessmentParamsBase64, "utf-8")
+
+    # build request
+    url = "https://%s.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=%s&usePipelineVersion=0" % (region, language)
+    headers = { 'Accept': 'application/json;text/xml',
+                'Connection': 'Keep-Alive',
+                'Content-Type': 'audio/wav; codecs=audio/pcm; samplerate=16000',
+                'Ocp-Apim-Subscription-Key': subscription_key,
+                'Pronunciation-Assessment': pronAssessmentParams,
+                'Transfer-Encoding': 'chunked',
+                'Expect': '100-continue' }
+
+    #audioFile = open('audio.wav', 'rb')
+    audioFile = f
+    # send request with chunked data
+    response = requests.post(url=url, data=get_chunk(audioFile), headers=headers)
+    #getResponseTime = time.time()
+    audioFile.close()
+
+    #latency = getResponseTime - uploadFinishTime
+    #print("Latency = %sms" % int(latency * 1000))
+
+    return response.json()
+
+
 
 # Get score from recently uploaded file
 @app.route('/get_score/<audio_id>/', methods=['GET'])
@@ -41,11 +118,6 @@ def get_score(audio_id):
     return str(round(100 * scoring_functions_withVAD.score_pronunciation(proper_series, user_series))) + '%'
 
 
-# Dummy response to satisfy website if it does get request to .../favicon.ico
-@app.route('/favicon.ico', methods=['GET'])
-def favicon():
-    return '<h1></h1>'
-
 
 # Navigation to url will generate random choice and return to HTML
 @app.route('/get_random_line', methods=['GET'])
@@ -53,12 +125,6 @@ def get_random_line():
     path = Path(__file__).parent.__str__() + "\\reference_files.txt"
     lines = open(path, encoding="UTF-8").readlines()[1:]
     return random.choice(lines)
-
-
-# Home page, render the "index.html" template
-@app.route('/')
-def home():
-    return render_template('index.html', name=None)
 
 
 # Navigation will create GET request for website and POST request for audio data as mp3 file
